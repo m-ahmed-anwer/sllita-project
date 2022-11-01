4792ba353a74e7dc94b09ab486f71e0d810fa4ae
m-ahmed-anwer <112756273+m-ahmed-anwer@users.noreply.github.com>
2022-11-01 22:21:03 +0530

    Hi

--- Assignment1/src/Customer/Customer.java
+++ Assignment1/src/Customer/Customer.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Customer;
 
-/**
- *
- * @author K_R_I_S_H_A_N_T_H_A
- */
 public class Customer {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hello");
     }
