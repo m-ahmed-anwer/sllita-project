9f73ee164d66dbceb3f0484c5a0f888950b8f56b
Krishantha2001 <117063554+Krishantha2001@users.noreply.github.com>
2022-11-01 21:57:21 +0530

    Add krishantha

---
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author K_R_I_S_H_A_N_T_H_A
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    //iuyasiufhiuahfiuhaiufhiusafhiusafiuhwsiuofhwiouegf
+}
