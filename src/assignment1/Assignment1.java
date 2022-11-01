4985aed8fd0aff0e53d7a6f08d1a89804cb00113
m-ahmed-anwer <112756273+m-ahmed-anwer@users.noreply.github.com>
2022-11-01 21:39:29 +0530

    Created a Customer class

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        
+        Customer c1= new Customer(1,"Ahmed","ahmedanwer@gmail.com","0768242884");
+        
+    }
+    
+}
---
+++ src/assignment1/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment1;
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNum;
+
+    public Customer(int id, String name, String email, String phoneNum) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNum = phoneNum;
+    }
+    
+    
+    
+}
