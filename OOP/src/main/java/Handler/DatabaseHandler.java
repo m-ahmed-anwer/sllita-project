1f1032543f4ad057c236218f770de7cb84998a55
m-ahmed-anwer <112756273+m-ahmed-anwer@users.noreply.github.com>
2022-12-03 15:41:08 +0530

    Message Change

---
+++ OOP/src/main/java/Handler/DatabaseHandler.java
@@ -0,0 +1,19 @@
+
+package Handler;
+
+import DatabaseLayer.DatabaseConnect;
+import Models.Customer;
+
+/**
+ *
+ * @author ahmed
+ */
+public class DatabaseHandler {
+    
+    DatabaseConnect d1= new DatabaseConnect();
+    
+    void addCustomer(Customer c1){
+         
+    }
+            
+}
---
+++ OOP/src/main/java/Models/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author ahmed
+ */
+public class Login {
+    String username;
+    String password;
+
+    public Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+}
